82fcc6c86ca74577c64c4c63977d1893c4ebc4dd
Syphax <gs_bouazzouni@esi.dz>
2024-10-28 09:10:42 +0100

    Updates

---
+++ _next/static/f2tP9Oe8hE9JjA2O9wfhP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-8ec9a1983a76c7f2.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
