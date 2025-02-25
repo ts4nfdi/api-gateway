85112d2a2a123bdc52a50db0c05372ff08bba38b
Syphax bouazzouni <gs_bouazzouni@esi.dz>
2025-02-25 12:08:34 +0100

    Feature: Add authentification UIs (#44)
    * add frontend
    
    * clean next default global css
    
    * tmp commit
    
    * tmp
    
    * fix removed main page
    
    * create modal utils to handle opening and closing modal
    
    * clean auth and collections controllers and catch better exception
    
    * add tailwind and shadcn
    
    * add new header design
    
    * add .env.sample
    
    * add shadcn UI components
    
    * updating the homepage design migrating to shadcn ui
    
    * add a placeholder for the login page
    
    * add key for selector component
    
    * update UI theme
    
    * factorise header component to extract banner
    
    * add authentication guard and login and register pages

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -1,22 +1,28 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
 import './globals.css'
+import {AuthProvider} from "@/lib/authGuard";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'Widgets Demo',
-  description: 'Widgets demo',
+    title: 'Widgets Demo',
+    description: 'Widgets demo',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className={inter.className + " bg-gray-100"}>
+        <AuthProvider
+            config={{loginRedirect: '/auth/profile', guestRedirect: '/auth/profile', authRedirect: '/auth/login'}}>
+            {children}
+        </AuthProvider>
+        </body>
+        </html>
+    )
 }
