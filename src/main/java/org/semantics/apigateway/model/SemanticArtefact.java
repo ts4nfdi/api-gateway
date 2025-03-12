24d46b593f32fb99759330ed8139cc047da24299
Syphax <gs_bouazzouni@esi.dz>
2025-03-12 09:15:05 +0100

    add in the semantic artefact model the required metadata of MOD

--- src/main/java/org/semantics/apigateway/model/SemanticArtefact.java
+++ src/main/java/org/semantics/apigateway/model/SemanticArtefact.java
@@ -1,14 +1,39 @@
 package org.semantics.apigateway.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import java.util.List;
 
 @Data
 public class SemanticArtefact {
+    @JsonProperty("@id")
+    private String id;
+    @JsonProperty("@type")
+    private String type;
+
     private String acronym;
-    private String URI;
     private String description;
-    private String issued;
+    private String versionIRI;
+    private String accessRights;
+    private String license;
+    private String contactPoint;
+    private String creator;
+    private String identifier;
+    private String keyword;
+    private String landingPage;
+    private String language;
+    private String publisher;
+    private String subject;
     private String title;
-    private List<String> sources;
+    private String accrualMethod;
+    private String accrualPeriodicity;
+    private String bibliographicCitation;
+    private String contributor;
+    private String coverage;
+    private String hasFormat;
+    private String rightsHolder;
+    private String competencyQuestion;
+    private String semanticArtefactRelation;
+    private String createdWith;
+    private String wasGeneratedBy;
+    private String includedInDataCatalog;
 }
