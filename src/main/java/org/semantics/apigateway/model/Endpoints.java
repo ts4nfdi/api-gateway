3b89f42184bcb53b1c62306d0a57a910dcbddcb1
Syphax <gs_bouazzouni@esi.dz>
2025-03-27 23:29:53 +0100

    fix: schemes endpoint internal server

--- src/main/java/org/semantics/apigateway/model/Endpoints.java
+++ src/main/java/org/semantics/apigateway/model/Endpoints.java
@@ -1,7 +1,7 @@
 package org.semantics.apigateway.model;
 
 public enum Endpoints {
-    resources,search, concepts, properties, collections, individuals,
+    resources,search, concepts, properties, collections, individuals, schemes,
     concept_details,resource_details, property_details, collection_details,individual_details, scheme_details;
 
     @Override
