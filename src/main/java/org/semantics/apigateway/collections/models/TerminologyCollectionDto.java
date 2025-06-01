06f6e71fc723029e07575f449ccf574327c758b3
Syphax <gs_bouazzouni@esi.dz>
2025-06-01 17:00:46 +0200

    update collection controller and create a dedicated service module

---
+++ src/main/java/org/semantics/apigateway/collections/models/TerminologyCollectionDto.java
@@ -0,0 +1,50 @@
+package org.semantics.apigateway.collections.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Data
+public class TerminologyCollectionDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    UUID id;
+    @NotNull
+    String label;
+    @NotNull
+    String description;
+    @NotNull
+    boolean isPublic;
+    List<CollectionCollaboratorDto> collaborators = Collections.emptyList();
+    List<ResourceDto> terminologies = Collections.emptyList();
+
+
+    public static TerminologyCollectionDto toDto(TerminologyCollection x) {
+        TerminologyCollectionDto dto = new TerminologyCollectionDto();
+        dto.setId(x.getId());
+        dto.setLabel(x.getLabel());
+        dto.setDescription(x.getDescription());
+        dto.setPublic(x.isPublic());
+        dto.setCollaborators(x.getCollaborators().stream().map(c -> {
+            return new CollectionCollaboratorDto(
+                    c.getUser().getUsername(),
+                    c.getRole()
+            );
+        }).toList());
+        dto.setTerminologies(x.getTerminologies().stream().map(r -> {
+            return new ResourceDto(
+                    r.getUri(),
+                    r.getLabel(),
+                    r.getSource(),
+                    r.getType()
+            );
+        }).toList());
+        return dto;
+    }
+}
+
