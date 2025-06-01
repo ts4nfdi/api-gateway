5ef2d56a307c584373e36560d5e4e690feb7de5d
Syphax <gs_bouazzouni@esi.dz>
2025-06-01 16:17:34 +0200

    add get all usernames endpoint

--- src/main/java/org/semantics/apigateway/controller/local/UsersController.java
+++ src/main/java/org/semantics/apigateway/controller/local/UsersController.java
@@ -6,10 +6,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.semantics.apigateway.model.user.User;
 import org.semantics.apigateway.service.auth.AuthService;
+import org.semantics.apigateway.service.auth.UserRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -20,9 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsersController {
 
     private final AuthService authService;
+    private final UserRepository userRepository;
 
     @GetMapping("/me")
     public User getCurrentUser() {
         return this.authService.getCurrentUser();
+    }
+
+    @GetMapping("/")
+    public List<Map<String, String>> getAllUsers() {
+        return this.userRepository.findAll().stream().map(x -> Map.of(
+                "username", x.getUsername()
+        )).toList();
     }
 }
