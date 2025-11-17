2ec419caaaf2184974cdc274746c79d09cc7c51e
Ralph Schaefermeier <ralph.schafermeier@gmail.com>
2025-11-17 09:08:11 +0100

    Add OLS endpoints

--- src/main/java/org/semantics/apigateway/controller/ols/OlsSearchController.java
+++ src/main/java/org/semantics/apigateway/controller/ols/OlsSearchController.java
@@ -1,12 +1,16 @@
 package org.semantics.apigateway.controller.ols;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.ws.rs.QueryParam;
 import org.semantics.apigateway.artefacts.data.ArtefactsDataService;
+import org.semantics.apigateway.artefacts.metadata.ArtefactsService;
 import org.semantics.apigateway.artefacts.search.SearchService;
 import org.semantics.apigateway.model.CommonRequestParams;
 import org.semantics.apigateway.model.TargetDbSchema;
 import org.semantics.apigateway.model.responses.AggregatedApiResponse;
+import org.semantics.apigateway.model.user.User;
+import org.semantics.apigateway.service.auth.AuthService;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,11 +22,15 @@ import java.util.Map;
 public class OlsSearchController {
 
     private final SearchService searchService;
-    private final ArtefactsDataService artefactsService;
+    private final ArtefactsService artefactsService;
+    private final ArtefactsDataService artefactsDataService;
+    private final AuthService authService;
 
-    public OlsSearchController(SearchService searchService, ArtefactsDataService artefactsService) {
+    public OlsSearchController(SearchService searchService, ArtefactsService artefactsService, ArtefactsDataService artefactsDataService, AuthService authService) {
         this.searchService = searchService;
         this.artefactsService = artefactsService;
+        this.artefactsDataService = artefactsDataService;
+        this.authService = authService;
     }
 
     @CrossOrigin
@@ -40,13 +48,29 @@ public class OlsSearchController {
         }
 
         AggregatedApiResponse response = searchService.performSearch(query + "*", allParams.get("database"), "ols", false);
-        return response.getCollection().get(0);
+        return response.getCollection().getFirst();
     }
 
     @CrossOrigin
     @GetMapping("/api/ontologies/{ontology}/terms")
     public Object getTermsInOLSTargetDBSchema(@PathVariable String ontology, @QueryParam("iri") String iri, @ParameterObject CommonRequestParams params) {
         params.setTargetDbSchema(TargetDbSchema.ols);
-        return this.artefactsService.getArtefactTerm(ontology, iri, params,  null);
+        return this.artefactsDataService.getArtefactTerm(ontology, iri, params,  null);
     }
+  
+  @CrossOrigin
+  @GetMapping("/api/ontologies/{ontology}")
+  public Object getArtefactMetadataInOLSTargetDBSchema(@PathVariable String ontology, @ParameterObject CommonRequestParams params) {
+    params.setTargetDbSchema(TargetDbSchema.ols);
+    return this.artefactsService.getArtefact(ontology, params, null);
+  }
+  
+  @CrossOrigin
+  @GetMapping("/api/ontologies")
+  public Object getArtefactsInOLSTargetDBSchema(@ParameterObject CommonRequestParams params,
+                                                @Parameter(description = "Collection id to browse terminologies in") @RequestParam(required = false) String collectionId) {
+    params.setTargetDbSchema(TargetDbSchema.ols);
+    User user = authService.tryGetCurrentUser();
+    return this.artefactsService.getArtefacts(params, collectionId, user, null);
+  }
 }
